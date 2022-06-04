886a72956dbf192c9ae7564a05fa98823746ffce
Serhat Merak <merakserhat@gmail.com>
2022-06-04 21:14:47 +0300

    create routes and controllers

--- BilFoot_backend/index.js
+++ BilFoot_backend/index.js
@@ -4,6 +4,14 @@ import mongoose from "mongoose";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+// #region route imports
+import authRoute from "./routes/auth_r.js";
+import announcementRoute from "./routes/announcement_r.js";
+import chatRoute from "./routes/chat_r.js";
+import playerRoute from "./routes/player_r.js";
+import teamRoute from "./routes/team_r.js";
+
+// #endregion
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -18,6 +26,14 @@ app.post("/test", (req, res, next) => {
   console.log(req.body);
   res.status(200).json({ message: "Successfully aldın mesajı!" });
 });
+
+// #region route registers
+app.use("/auth", authRoute);
+app.use("/user", announcementRoute);
+app.use("/product", chatRoute);
+app.use("/page", playerRoute);
+app.use("/test", teamRoute);
+// #endregion
 
 app.listen(8080);
 /*
