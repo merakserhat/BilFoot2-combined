72ec3dee8bc717d9a59eb0e05f20156949d337ed
Serhat Merak <merakserhat@gmail.com>
2022-07-10 13:59:27 +0300

    feat(Be): connect to mongodb atlas

--- BilFoot_backend/index.ts
+++ BilFoot_backend/index.ts
@@ -13,6 +13,8 @@ import teamRoute from "./routes/team_r";
 import { getApp, initializeApp } from "firebase-admin/app";
 import admin from "firebase-admin";
 
+require("dotenv").config();
+
 //run this before npm start
 //export GOOGLE_APPLICATION_CREDENTIALS="/Users/serhatmerak/Documents/GitHub/BilFoot2/BilFoot_backend/firebase/serviceAccountKey.json"
 
@@ -102,18 +104,19 @@ app.use("/user", announcementRoute);
 app.use("/product", chatRoute);
 app.use("/page", playerRoute);
 app.use("/test", teamRoute);
-// #endregion
+// #endregion<
 
-app.listen(8080);
-/*
+// app.listen(8080);
+
+console.log(process.env.MONGODB_URI);
+
 mongoose
-    .connect(process.env.MONGODB_URI)
-    .then((result) => {
-        // console.log(result);
-        console.log("server is active");
-    })
-    .catch((err) => {
-        console.log("db error");
-        // console.log(err);
-    });
-    */
+  .connect(process.env.MONGODB_URI || "error")
+  .then((result) => {
+    // console.log(result);
+    console.log("server is active");
+  })
+  .catch((err) => {
+    console.log("db error");
+    console.log(err);
+  });
