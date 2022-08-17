7260f7cc93af509177af37d280018d7141d51049
At1llaes22 <91745094+At1llaes22@users.noreply.github.com>
2022-08-17 15:38:52 +0300

    Feature/base-modal (#2)
    * feat(fe): added new modals
    
    * feat(fe): little adjustments to modals
    
    * feat(fe): more small adjusments

---
+++ BilFoot_flutter/bilfoot/lib/views/widgets/modals/captain_modal.dart
@@ -0,0 +1,33 @@
+import 'package:bilfoot/views/widgets/modals/base_modal.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CaptainModal extends StatelessWidget {
+  const CaptainModal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseModal(
+      type: Type.ANSWERABLE,
+      text: "Oyuncuyu kaptan yapmak istiyor musun?",
+      //icon: //findicon,
+      accepButtonText: "Evet",
+      onAccepted: () {
+        //TODO:
+      },
+      onRefused: () {
+        //TODO:
+      },
+      refuseButtonText: "Hayır",
+      child: const Text(
+        "C",
+        style: TextStyle(
+          decoration: TextDecoration.none,
+          fontFamily: "assets/fonts/Poppins-ExtraBold.ttf",
+          color: Colors.orange,
+        ),
+      ),
+    );
+  }
+}
