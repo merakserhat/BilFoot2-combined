d576927757a1d9918dc8deb108b41d18807706ec
At1llaes22 <at1llaes22@gmail.com>
2022-08-26 15:15:24 +0300

    Feat(Fe): designed match info

--- BilFoot_flutter/bilfoot/lib/views/screens/match_page/widgets/match_info.dart
+++ BilFoot_flutter/bilfoot/lib/views/screens/match_page/widgets/match_info.dart
@@ -1,5 +1,7 @@
 import 'package:bilfoot/config/utils/hex_color.dart';
 import 'package:bilfoot/data/models/match_model.dart';
+import '../../../themes/my_themes.dart';
+import 'match_list_item.dart';
 import 'package:bilfoot/data/models/team_model.dart';
 import 'package:flutter/material.dart';
 
@@ -12,18 +14,95 @@ class MatchInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12),
-      child: Column(
+      child: Expanded(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            matchModel.isPitchApproved
+                ? const Icon(
+                    Icons.check_circle_outline,
+                    color: Colors.green,
+                    size: 32,
+                  )
+                : const Icon(
+                    Icons.warning_amber,
+                    color: Colors.amber,
+                    size: 32,
+                  ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  matchModel.hour,
+                  textAlign: TextAlign.end,
+                  style: Theme.of(context).textTheme.headline2!.copyWith(
+                      color: Colors.black87, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  matchModel.date,
+                  textAlign: TextAlign.start,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline2!
+                      .copyWith(color: Colors.black87),
+                ),
+              ],
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 20,
+                  child: Image.asset(
+                    "assets/images/pitch.png",
+                    color: Colors.green,
+                  ),
+                ),
+                const SizedBox(
+                  width: 1,
+                ),
+                Text(
+                  matchModel.pitch,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline2,
+                ),
+              ],
+            ),
+            Text(
+              '${matchModel.people.length}/${matchModel.peopleLimit}',
+              style: Theme.of(context)
+                  .textTheme
+                  .headline2!
+                  .copyWith(color: _getColor(), fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+
+      /*Column(
         children: [
           Text(
             '${matchModel.date} ${matchModel.hour}',
-            style: Theme.of(context).textTheme.headline1,
+            style: Theme.of(context).textTheme.headline2,
           ),
           Text(
             matchModel.pitch,
-            style: Theme.of(context).textTheme.headline1,
+            style: Theme.of(context).textTheme.headline2,
           ),
         ],
-      ),
+      ),*/
     );
+  }
+
+  Color _getColor() {
+    if (matchModel.people.length == matchModel.peopleLimit) {
+      return Colors.red;
+    } else if (matchModel.people.length >= matchModel.peopleLimit - 3) {
+      return Colors.orange;
+    } else {
+      return MyThemes.darkTheme.primaryColor;
+    }
   }
 }
