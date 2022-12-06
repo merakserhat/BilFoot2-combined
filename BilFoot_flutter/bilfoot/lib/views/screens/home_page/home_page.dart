48205de6d2d821a7ac46ea784024566dc7fc687d
Serhat Merak <42006836+merakserhat@users.noreply.github.com>
2022-12-06 23:07:57 +0300

    feat: clear home page

--- BilFoot_flutter/bilfoot/lib/views/screens/home_page/home_page.dart
+++ BilFoot_flutter/bilfoot/lib/views/screens/home_page/home_page.dart
@@ -1,11 +1,5 @@
 import 'package:bilfoot/config/constants/program_constants.dart';
-import 'package:bilfoot/views/screens/phone_number_page/widgets/phone_number_input.dart';
-import 'package:bilfoot/views/screens/phone_number_page/widgets/phone_privacy_settings.dart';
-import 'package:bilfoot/views/widgets/bilfoot_button.dart';
-import 'package:bilfoot/views/widgets/modals/set_phone_modal.dart';
-import 'package:bilfoot/views/widgets/whatsapp_button.dart';
 import "package:flutter/material.dart";
-import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -27,41 +21,9 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
         body: Padding(
       padding: ProgramConstants.pagePadding,
-      child: Form(
-        key: formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox.square(dimension: 20),
-            Text(
-              "Your Phone Number",
-              style: Theme.of(context).textTheme.headline1,
-            ),
-            const SizedBox.square(dimension: 20),
-            /*PhoneNumberInput(
-              initialNumber: "+905455591600",
-            ),*/
-            const SizedBox.square(dimension: 60),
-            Text(
-              "Only those people can reach your phone number:\n\n\t-Your teammates.\n\t-Players from your matches\n\t-Players you have interacted via announcements",
-              style: Theme.of(context).textTheme.bodyText2,
-            ),
-            const SizedBox.square(dimension: 60),
-            BilfootButton(
-              label: "Update",
-              onPressed: () {
-                ProgramConstants.showBlurryBackground(
-                    context: context,
-                    child: const SetPhoneModal(
-                      phoneModalText: PhoneModalText.whatsappButtonText,
-                    ));
-              },
-            ),
-            const WhatsappButton(
-              userId: "62f01e8381c097b01e4d777b",
-            ),
-          ],
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: const [Text("Home Page")],
       ),
     ));
   }
